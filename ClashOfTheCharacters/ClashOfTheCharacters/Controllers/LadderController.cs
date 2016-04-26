77836474c8db66b68038da68deaeca9db90edff3
Rickard W <rickard.wiesner@hotmail.com>
2016-04-26 13:09:47 +0200

    Städat lite efter Kalle did his thang.

--- ClashOfTheCharacters/ClashOfTheCharacters/Controllers/LadderController.cs
+++ ClashOfTheCharacters/ClashOfTheCharacters/Controllers/LadderController.cs
@@ -13,32 +13,10 @@ namespace ClashOfTheCharacters.Controllers
         // GET: Ladder
         public ActionResult Index()
         {
-            string userSearch = Request["searchField"] == null ? "" : Request["searchField"];
+            //Om det finns någon sträng i sökbaren sök, annars visa alla användare,
+            string userSearch = Request["SearchInput"] == null ? "" : Request["SearchInput"];
 
             return View(context.Users.Where(c => c.UserName.Contains(userSearch)).OrderByDescending(c => c.Rank));
-        }
-        //[HttpPost]
-        //public ActionResult SortByName()
-        //{
-        //    var userList = context.Users.ToList();
-        //    var orderedByName = userList.OrderBy(x => x.UserName);
-        //    return View(orderedByName.ToList());
-        //}
-        [HttpPost]
-        public ActionResult SearchForUser()
-        {
-            var userList = context.Users.ToList();
-            List<ApplicationUser> searchList = new List<ApplicationUser>();
-            string userSearch = Request["searchField"];
-
-            foreach (var user in context.Users.ToList())
-            {
-                if (user.UserName.ToLower().Contains(userSearch.ToLower()))
-                {
-                    searchList.Add(user);
-                }
-            }
-            return View(searchList);
         }
     }
 }
