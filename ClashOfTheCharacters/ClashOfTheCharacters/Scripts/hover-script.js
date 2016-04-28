485aa27a199faf20dce9369612674246c9be9695
Calle Sandstrom <callesandstrom@hotmail.com>
2016-04-28 21:16:39 +0200

    *Äntligen en hover som "funkar"

---
+++ ClashOfTheCharacters/ClashOfTheCharacters/Scripts/hover-script.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('div.dropdown').hover(function () {
+        $('.dropdown-menu', this).fadeIn('fast');
+
+    }, function () {
+        $('.dropdown-menu', this).fadeOut('fast');
+    });
+});
