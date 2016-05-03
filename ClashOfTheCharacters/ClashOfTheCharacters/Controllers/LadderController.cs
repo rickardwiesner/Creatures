aa4ef19f91015afe28118b98b7af629eb762fbf2
Calle Sandstrom <callesandstrom@hotmail.com>
2016-05-03 15:51:42 +0200

    *Battle mot datorn

--- ClashOfTheCharacters/ClashOfTheCharacters/Controllers/LadderController.cs
+++ ClashOfTheCharacters/ClashOfTheCharacters/Controllers/LadderController.cs
@@ -9,14 +9,26 @@ namespace ClashOfTheCharacters.Controllers
 {
     public class LadderController : Controller
     {
-        ApplicationDbContext context = new ApplicationDbContext();
+        ApplicationDbContext db = new ApplicationDbContext();
         // GET: Ladder
         public ActionResult Index()
         {
             //Om det finns någon sträng i sökbaren sök, annars visa alla användare,
-            string userSearch = Request["SearchInput"] == null ? "" : Request["SearchInput"];
-            var users = context.Users.Where(c => c.UserName.ToLower().Contains(userSearch.ToLower())).OrderByDescending(c => c.LadderPoints).ToList();
-            return View(users);
+            if (string.IsNullOrEmpty(Request["SearchInput"] ))
+            {
+                ViewBag.Search = false;
+
+                return View(db.Users.OrderByDescending(u => u.LadderPoints).ToList());
+            }
+
+            else
+            {
+                var userSearch = Request["SearchInput"];
+
+                ViewBag.Search = true;
+
+                return View(db.Users.Where(c => c.UserName.ToLower().Contains(userSearch.ToLower())).OrderByDescending(c => c.LadderPoints).ToList());
+            }
         }
     }
 }
