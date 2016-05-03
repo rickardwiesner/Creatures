1cb1f27029d9222dd54bb9387b7507452bc8d75f
Calle Sandstrom <callesandstrom@hotmail.com>
2016-05-03 10:37:58 +0200

    *WildBattle

---
+++ ClashOfTheCharacters/ClashOfTheCharacters/Models/WildBattle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClashOfTheCharacters.Models
+{
+    public class WildBattle
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public int StageId { get; set; }
+        public virtual Stage Stage { get; set; }
+
+        public virtual ICollection<WildBattleCreature> WildBattleCreatures { get; set; }
+    }
+}
---
+++ ClashOfTheCharacters/ClashOfTheCharacters/Models/WildBattleAction.cs
@@ -0,0 +1,32 @@
+﻿using ClashOfTheCharacters.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClashOfTheCharacters.Models
+{
+    public class WildBattleAction
+    {
+        public int Id { get; set; }
+
+        public int? Damage { get; set; }
+
+        public bool FinishingBlow { get { return Defender.Hp == 0 ? true : false; } }
+
+        public Effect Effect { get; set; }
+
+        public int WildBattleId { get; set; }
+        public virtual WildBattle WildBattle { get; set; }
+
+        public int? AttackerId { get; set; }
+        public virtual WildBattleCreature Attacker { get; set; }
+
+        public int? DefenderId { get; set; }
+        public virtual WildBattleCreature Defender { get; set; }
+
+        public bool CaptureAttempt { get; set; }
+
+        public bool? CaptureSuccess { get; set; }
+    }
+}
