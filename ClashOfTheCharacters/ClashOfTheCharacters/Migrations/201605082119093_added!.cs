7639b872753fed9baed0722514f224eb0e81b28f
Omran <palman115@hotmail.com>
2016-05-08 23:56:51 +0200

    css_home,HomeIndex,characters bytas till creature in (home/Chara.../index)

--- ClashOfTheCharacters/ClashOfTheCharacters/Migrations/201605082119093_added!.cs
+++ ClashOfTheCharacters/ClashOfTheCharacters/Migrations/201605082119093_added!.cs
@@ -3,7 +3,7 @@ namespace ClashOfTheCharacters.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class InitialCreate : DbMigration
+    public partial class added : DbMigration
     {
         public override void Up()
         {
@@ -102,11 +102,15 @@ namespace ClashOfTheCharacters.Migrations
                         ImageUrl = c.String(),
                         Stamina = c.Int(nullable: false),
                         MaxStamina = c.Int(nullable: false),
+                        RainbowGems = c.Int(nullable: false),
                         Gold = c.Int(nullable: false),
                         Xp = c.Int(nullable: false),
                         Level = c.Int(nullable: false),
                         LadderPoints = c.Int(nullable: false),
                         Admin = c.Boolean(nullable: false),
+                        Battles = c.Int(nullable: false),
+                        Wins = c.Int(nullable: false),
+                        Losses = c.Int(nullable: false),
                         LastActive = c.DateTimeOffset(nullable: false, precision: 7),
                         LastStaminaTime = c.DateTimeOffset(nullable: false, precision: 7),
                         Email = c.String(maxLength: 256),
@@ -123,6 +127,83 @@ namespace ClashOfTheCharacters.Migrations
                     })
                 .PrimaryKey(t => t.Id)
                 .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AuctionTargets",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        AuctionCreatureId = c.Int(nullable: false),
+                        UserId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AuctionCreatures", t => t.AuctionCreatureId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId)
+                .Index(t => t.AuctionCreatureId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AuctionCreatures",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        StartPrice = c.Int(nullable: false),
+                        CurrentBid = c.Int(),
+                        BuyoutPrice = c.Int(),
+                        Finished = c.Boolean(nullable: false),
+                        EndTime = c.DateTimeOffset(nullable: false, precision: 7),
+                        UserCreatureId = c.Int(nullable: false),
+                        OwnerId = c.String(maxLength: 128),
+                        CurrentBidderId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.CurrentBidderId)
+                .ForeignKey("dbo.AspNetUsers", t => t.OwnerId)
+                .ForeignKey("dbo.UserCreatures", t => t.UserCreatureId, cascadeDelete: true)
+                .Index(t => t.UserCreatureId)
+                .Index(t => t.OwnerId)
+                .Index(t => t.CurrentBidderId);
+            
+            CreateTable(
+                "dbo.UserCreatures",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Level = c.Int(nullable: false),
+                        Xp = c.Int(nullable: false),
+                        Slot = c.Int(nullable: false),
+                        Battles = c.Int(nullable: false),
+                        Kills = c.Int(nullable: false),
+                        Deaths = c.Int(nullable: false),
+                        InSquad = c.Boolean(nullable: false),
+                        InAuction = c.Boolean(nullable: false),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        CreatureId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Creatures", t => t.CreatureId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.CreatureId);
+            
+            CreateTable(
+                "dbo.Creatures",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 9),
+                        Element = c.Int(nullable: false),
+                        ImageUrl = c.String(),
+                        Price = c.Int(nullable: false),
+                        Rarity = c.Int(nullable: false),
+                        BaseAttack = c.Int(nullable: false),
+                        BaseDefense = c.Int(nullable: false),
+                        BaseHp = c.Int(nullable: false),
+                        AttackMultiplier = c.Single(nullable: false),
+                        DefenseMultiplier = c.Single(nullable: false),
+                        HpMultiplier = c.Single(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
             
             CreateTable(
                 "dbo.AspNetUserClaims",
@@ -159,23 +240,12 @@ namespace ClashOfTheCharacters.Migrations
                         Element = c.Int(nullable: false),
                         Cost = c.Int(nullable: false),
                         Hours = c.Int(nullable: false),
+                        Levels = c.Int(nullable: false),
+                        GoldReward = c.Int(nullable: false),
+                        XpReward = c.Int(nullable: false),
                         ImageUrl = c.String(),
                     })
                 .PrimaryKey(t => t.Id);
-            
-            CreateTable(
-                "dbo.Stages",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        Index = c.Int(nullable: false),
-                        GoldReward = c.Int(nullable: false),
-                        XpReward = c.Int(nullable: false),
-                        LandId = c.Int(nullable: false),
-                    })
-                .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.Lands", t => t.LandId, cascadeDelete: true)
-                .Index(t => t.LandId);
             
             CreateTable(
                 "dbo.AspNetUserLogins",
@@ -217,44 +287,53 @@ namespace ClashOfTheCharacters.Migrations
                 .Index(t => t.LandId);
             
             CreateTable(
-                "dbo.UserCreatures",
+                "dbo.UserItems",
                 c => new
                     {
                         Id = c.Int(nullable: false, identity: true),
-                        Level = c.Int(nullable: false),
-                        Xp = c.Int(nullable: false),
-                        Slot = c.Int(nullable: false),
-                        Battles = c.Int(nullable: false),
-                        Kills = c.Int(nullable: false),
-                        Deaths = c.Int(nullable: false),
-                        InSquad = c.Boolean(nullable: false),
                         UserId = c.String(nullable: false, maxLength: 128),
-                        CreatureId = c.Int(nullable: false),
+                        ItemId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        InBag = c.Boolean(nullable: false),
                     })
                 .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.Creatures", t => t.CreatureId, cascadeDelete: true)
+                .ForeignKey("dbo.Items", t => t.ItemId, cascadeDelete: true)
                 .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
                 .Index(t => t.UserId)
-                .Index(t => t.CreatureId);
+                .Index(t => t.ItemId);
             
             CreateTable(
-                "dbo.Creatures",
+                "dbo.Items",
                 c => new
                     {
                         Id = c.Int(nullable: false, identity: true),
                         Name = c.String(),
-                        Element = c.Int(nullable: false),
-                        ImageUrl = c.String(),
                         Price = c.Int(nullable: false),
+                        ItemType = c.Int(nullable: false),
                         Rarity = c.Int(nullable: false),
-                        BaseAttack = c.Int(nullable: false),
-                        BaseDefense = c.Int(nullable: false),
-                        BaseHp = c.Int(nullable: false),
-                        AttackMultiplier = c.Single(nullable: false),
-                        DefenseMultiplier = c.Single(nullable: false),
-                        HpMultiplier = c.Single(nullable: false),
                     })
                 .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.AuctionItems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        StartPrice = c.Int(nullable: false),
+                        CurrentBid = c.Int(),
+                        BuyoutPrice = c.Int(),
+                        TimeRemaining = c.DateTimeOffset(nullable: false, precision: 7),
+                        UserItemId = c.Int(nullable: false),
+                        OwnerId = c.String(maxLength: 128),
+                        CurrentBidderId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.CurrentBidderId)
+                .ForeignKey("dbo.AspNetUsers", t => t.OwnerId)
+                .ForeignKey("dbo.UserItems", t => t.UserItemId, cascadeDelete: true)
+                .Index(t => t.UserItemId)
+                .Index(t => t.OwnerId)
+                .Index(t => t.CurrentBidderId);
             
             CreateTable(
                 "dbo.CurrentLands",
@@ -262,7 +341,7 @@ namespace ClashOfTheCharacters.Migrations
                     {
                         Id = c.Int(nullable: false, identity: true),
                         WildCreatureStartLevel = c.Int(nullable: false),
-                        CurrentStageIndex = c.Int(nullable: false),
+                        CurrentLevel = c.Int(nullable: false),
                         UserId = c.String(maxLength: 128),
                         LandId = c.Int(nullable: false),
                     })
@@ -346,15 +425,15 @@ namespace ClashOfTheCharacters.Migrations
                     {
                         Id = c.Int(nullable: false, identity: true),
                         UserId = c.String(maxLength: 128),
-                        StageId = c.Int(nullable: false),
+                        LandId = c.Int(nullable: false),
                         Won = c.Boolean(nullable: false),
                         Finished = c.Boolean(nullable: false),
                     })
                 .PrimaryKey(t => t.Id)
-                .ForeignKey("dbo.Stages", t => t.StageId, cascadeDelete: true)
+                .ForeignKey("dbo.Lands", t => t.LandId, cascadeDelete: true)
                 .ForeignKey("dbo.AspNetUsers", t => t.UserId)
                 .Index(t => t.UserId)
-                .Index(t => t.StageId);
+                .Index(t => t.LandId);
             
         }
         
@@ -365,7 +444,7 @@ namespace ClashOfTheCharacters.Migrations
             DropForeignKey("dbo.WildBattleCreatures", "WildBattleId", "dbo.WildBattles");
             DropForeignKey("dbo.WildBattleActions", "WildBattleId", "dbo.WildBattles");
             DropForeignKey("dbo.WildBattles", "UserId", "dbo.AspNetUsers");
-            DropForeignKey("dbo.WildBattles", "StageId", "dbo.Stages");
+            DropForeignKey("dbo.WildBattles", "LandId", "dbo.Lands");
             DropForeignKey("dbo.WildBattleCreatures", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.WildBattleCreatures", "CreatureId", "dbo.Creatures");
             DropForeignKey("dbo.Travels", "UserId", "dbo.AspNetUsers");
@@ -373,6 +452,9 @@ namespace ClashOfTheCharacters.Migrations
             DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
             DropForeignKey("dbo.CurrentLands", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.CurrentLands", "LandId", "dbo.Lands");
+            DropForeignKey("dbo.AuctionItems", "UserItemId", "dbo.UserItems");
+            DropForeignKey("dbo.AuctionItems", "OwnerId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AuctionItems", "CurrentBidderId", "dbo.AspNetUsers");
             DropForeignKey("dbo.Attacks", "DefenderId", "dbo.BattleCreatures");
             DropForeignKey("dbo.Attacks", "AttackerId", "dbo.BattleCreatures");
             DropForeignKey("dbo.BattleCreatures", "CreatureId", "dbo.Creatures");
@@ -382,18 +464,24 @@ namespace ClashOfTheCharacters.Migrations
             DropForeignKey("dbo.Battles", "ChallengeId", "dbo.Challenges");
             DropForeignKey("dbo.Challenges", "ReceiverId", "dbo.AspNetUsers");
             DropForeignKey("dbo.Challenges", "ChallengerId", "dbo.AspNetUsers");
-            DropForeignKey("dbo.UserCreatures", "UserId", "dbo.AspNetUsers");
-            DropForeignKey("dbo.UserCreatures", "CreatureId", "dbo.Creatures");
+            DropForeignKey("dbo.UserItems", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.UserItems", "ItemId", "dbo.Items");
             DropForeignKey("dbo.UnlockedLands", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.UnlockedLands", "LandId", "dbo.Lands");
             DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.ClearedLands", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.ClearedLands", "LandId", "dbo.Lands");
-            DropForeignKey("dbo.Stages", "LandId", "dbo.Lands");
             DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AuctionTargets", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AuctionTargets", "AuctionCreatureId", "dbo.AuctionCreatures");
+            DropForeignKey("dbo.AuctionCreatures", "UserCreatureId", "dbo.UserCreatures");
+            DropForeignKey("dbo.UserCreatures", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.UserCreatures", "CreatureId", "dbo.Creatures");
+            DropForeignKey("dbo.AuctionCreatures", "OwnerId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AuctionCreatures", "CurrentBidderId", "dbo.AspNetUsers");
             DropForeignKey("dbo.Attacks", "BattleId", "dbo.Battles");
-            DropIndex("dbo.WildBattles", new[] { "StageId" });
+            DropIndex("dbo.WildBattles", new[] { "LandId" });
             DropIndex("dbo.WildBattles", new[] { "UserId" });
             DropIndex("dbo.WildBattleCreatures", new[] { "WildBattleId" });
             DropIndex("dbo.WildBattleCreatures", new[] { "UserId" });
@@ -406,17 +494,26 @@ namespace ClashOfTheCharacters.Migrations
             DropIndex("dbo.AspNetRoles", "RoleNameIndex");
             DropIndex("dbo.CurrentLands", new[] { "LandId" });
             DropIndex("dbo.CurrentLands", new[] { "UserId" });
-            DropIndex("dbo.UserCreatures", new[] { "CreatureId" });
-            DropIndex("dbo.UserCreatures", new[] { "UserId" });
+            DropIndex("dbo.AuctionItems", new[] { "CurrentBidderId" });
+            DropIndex("dbo.AuctionItems", new[] { "OwnerId" });
+            DropIndex("dbo.AuctionItems", new[] { "UserItemId" });
+            DropIndex("dbo.UserItems", new[] { "ItemId" });
+            DropIndex("dbo.UserItems", new[] { "UserId" });
             DropIndex("dbo.UnlockedLands", new[] { "LandId" });
             DropIndex("dbo.UnlockedLands", new[] { "UserId" });
             DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
             DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
             DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
-            DropIndex("dbo.Stages", new[] { "LandId" });
             DropIndex("dbo.ClearedLands", new[] { "LandId" });
             DropIndex("dbo.ClearedLands", new[] { "UserId" });
             DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.UserCreatures", new[] { "CreatureId" });
+            DropIndex("dbo.UserCreatures", new[] { "UserId" });
+            DropIndex("dbo.AuctionCreatures", new[] { "CurrentBidderId" });
+            DropIndex("dbo.AuctionCreatures", new[] { "OwnerId" });
+            DropIndex("dbo.AuctionCreatures", new[] { "UserCreatureId" });
+            DropIndex("dbo.AuctionTargets", new[] { "UserId" });
+            DropIndex("dbo.AuctionTargets", new[] { "AuctionCreatureId" });
             DropIndex("dbo.AspNetUsers", "UserNameIndex");
             DropIndex("dbo.Challenges", new[] { "ReceiverId" });
             DropIndex("dbo.Challenges", new[] { "ChallengerId" });
@@ -434,15 +531,19 @@ namespace ClashOfTheCharacters.Migrations
             DropTable("dbo.Travels");
             DropTable("dbo.AspNetRoles");
             DropTable("dbo.CurrentLands");
-            DropTable("dbo.Creatures");
-            DropTable("dbo.UserCreatures");
+            DropTable("dbo.AuctionItems");
+            DropTable("dbo.Items");
+            DropTable("dbo.UserItems");
             DropTable("dbo.UnlockedLands");
             DropTable("dbo.AspNetUserRoles");
             DropTable("dbo.AspNetUserLogins");
-            DropTable("dbo.Stages");
             DropTable("dbo.Lands");
             DropTable("dbo.ClearedLands");
             DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.Creatures");
+            DropTable("dbo.UserCreatures");
+            DropTable("dbo.AuctionCreatures");
+            DropTable("dbo.AuctionTargets");
             DropTable("dbo.AspNetUsers");
             DropTable("dbo.Challenges");
             DropTable("dbo.Battles");
